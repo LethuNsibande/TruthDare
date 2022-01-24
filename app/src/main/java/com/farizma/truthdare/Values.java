e43a576963bc9df5021b35c5c70569ce091d662c
Lethukuthula Nsibande <lethunsibande16@gmail.com>
2022-01-24 15:37:48 +0200

    Update Values.java
    Added truths and dares on the values.

--- app/src/main/java/com/farizma/truthdare/Values.java
+++ app/src/main/java/com/farizma/truthdare/Values.java
@@ -10,7 +10,17 @@ public class Values {
             "Describe the strangest dream you've ever had. Did you like it?",
             "The world ends next week, and you can do anything you want (even if it's illegal). What would you do?",
             "How far would you go to land the guy or girl of your dreams?",
-            "What is the most childish thing that you still do?"
+            "What is the most childish thing that you still do?",
+            "Did you ever drink before you were of the legal drinking age?",
+            "What is the craziest thing you have done while under the influence?",
+            "What was the last thing you searched for on your phone web browser?",
+            "If you had to choose between going naked or having your thoughts appear in thought bubbles above your head for everyone to read, which would you choose?",
+            "Have you ever walked in on your parents doing it?",
+            "After you’ve dropped a piece of food, what’s the longest time you’ve left it on the ground and then ate it?",
+            "Have you ever tasted a booger?",
+            "What’s the first thing you would do if you woke up one day as the opposite sex?",
+            "If someone heard you snoring, would they say it’s obnoxiously loud?",
+            "Have you ever peed in a pool full of people?"
     };
 
     String[] dares = new String[]{
@@ -22,6 +32,16 @@ public class Values {
             "Let the group choose three random things from the refrigerator and mix them together. Then you have to eat it.",
             "Dig through the trash and name everything you find.",
             "Call a NY-style pizza place and ask them what the difference is between NY pizza and “real” pizza.",
-            "Take a selfie with the toilet and post it online."
+            "Take a selfie with the toilet and post it online.",
+            "Let the group choose an item for you to brush your teeth with.",
+            "Write your name on the floor with your tongue.",
+            "Go outside and pick exactly 40 blades of grass with a pair of tweezers.",
+            "Stick a Hot Cheeto in your nose and leave it there for 5 minutes.",
+            "Open Facebook, go to the account of the first person you see, and like every post on their wall going back to a year.",
+            "Eat a whole piece of paper.",
+            "Lick a car tire.",
+            "Open your front door and howl like a wolf for 30 seconds.",
+            "Let the person to your right put duct tape on any part of your body they choose and rip it off.",
+            "I dare you to tie your hands to your ankles for the rest of the game."
     };
 }
